2b2e6405970a55efb3f80a25c558505337dfd492
nhu <nhule2031998@gmail.com>
2021-02-05 04:44:27 +0700

    commit version 1

---
+++ src/main/java/com/laptrinhjavaweb/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.laptrinhjavaweb.repository;
+
+import com.laptrinhjavaweb.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findOneByUserNameAndStatus(String name, int status);
+    Page<UserEntity> findByUserNameContainingIgnoreCaseOrFullNameContainingIgnoreCaseAndStatusNot(String userName, String fullName, int status,
+                                                                                                  Pageable pageable);
+    Page<UserEntity> findByStatusNot(int status, Pageable pageable);
+    long countByUserNameContainingIgnoreCaseOrFullNameContainingIgnoreCaseAndStatusNot(String userName, String fullName, int status);
+    long countByStatusNot(int status);
+    UserEntity findOneByUserName(String userName);
+}
